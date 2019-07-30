99760447a27e9c1e26bec77386e243b28e0bcc9d
Markus Cozowicz <marcozo@microsoft.com>
2019-07-30 14:33:35 +0200

    use service principal secret during build skip test if credentials are missing

--- python/tests/azure/storage/test_azure_storage.py
+++ python/tests/azure/storage/test_azure_storage.py
@@ -10,13 +10,18 @@ def test_subdir():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+@pytest.mark.skipif(os.environ.get('myserviceprincipal1') is None,
+                    reason='Environment variable myserviceprincipal1 must be set to service principals secret')
 def test_service_principal(test_subdir):
+
     ws = pyworkspace.Workspace()
 
     svc1 = ws['myvault1']
     assert len(svc1.get_secret("workspacetest1")) > 0
 
 
+@pytest.mark.skipif(os.environ.get('myserviceprincipal1') is None,
+                    reason='Environment variable myserviceprincipal1 must be set to service principals secret')
 def test_service_csv1(test_subdir):
     ws = pyworkspace.Workspace()
 
